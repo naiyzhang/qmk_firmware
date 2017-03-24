a3129771b325d2cffc0688916a025c152ea5d0ea
Naiyuan Zhang <pirateblanc@gmail.com>
2017-03-23 22:59:38 -0700

    Update alt for gaming layer.

--- keyboards/ergodox/keymaps/naiyuan/keymap.c
+++ keyboards/ergodox/keymaps/naiyuan/keymap.c
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| -/_  |           |  =/+ |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LCtrl |  Gui | LAlt | Left | Right|                                       |  Up  | Down |   [  |   ]  | Del  |
+ *   |LCtrl |  Gui | LAlt | LAlt | LAlt |                                       |  Up  | Down |   [  |   ]  | Del  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,--------------.
  *                                        | FN1  | Home |       | PgUp | FN1    |
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_ESC,
         KC_F10,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_MINS,
-        KC_LCTL,        KC_LGUI,      KC_LALT,KC_LEFT,KC_RGHT,
+        KC_LCTL,        KC_LGUI,      KC_LALT,KC_LALT,KC_LALT,
                                               KC_FN3, KC_HOME,
                                                               KC_END,
                                               KC_SPC, KC_LCTL,KC_LALT,
