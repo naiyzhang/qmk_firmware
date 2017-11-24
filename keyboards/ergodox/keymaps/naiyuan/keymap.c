dc4ca7c8c11b2d9fd818419aa509a8deb8934318
Naiyuan Zhang <pirateblanc@gmail.com>
2017-11-24 13:08:26 -0800

    Adding Mac OS layer and moving Game layer.

--- keyboards/ergodox/keymaps/naiyuan/keymap.c
+++ keyboards/ergodox/keymaps/naiyuan/keymap.c
@@ -5,8 +5,10 @@
 
 #define BASE 0 // Default Layer
 #define BAFN 1 // Fn Actions
-#define GAME 2 // Gaming Layer
-#define GAFN 3 // Gaming Fn Actions
+#define MCLR 2 // Mac layer
+#define MCFN 3 // Mac Fn Actions
+#define GAME 4 // Gaming Layer
+#define GAFN 5 // Gaming Fn Actions
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -40,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[BASE] = KEYMAP(  // layer 0 : default
+[BASE] = KEYMAP(
         // left hand
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_INS,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_ESC,
@@ -63,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Base Function Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
- * |         |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+ * |  FN5    |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |         |      |      | VolD | VolU | Mute |      |           |      |      |      |      |      |      | Pause  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -84,6 +86,91 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [BAFN] = KEYMAP(
        // left hand
+       KC_FN5 ,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F11,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_VOLD,KC_VOLU,KC_MUTE,KC_TRNS,
+       KC_CAPS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+                                       KC_TRNS,KC_TRNS,
+                                               KC_TRNS,
+                               KC_TRNS,KC_TRNS,KC_TRNS,
+       // right hand
+       KC_F12,  KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_PAUS,
+                KC_HOME, KC_PGDN,KC_PGUP, KC_END,  KC_TRNS, KC_PSCR,
+       KC_TRNS, KC_MSTP, KC_MPLY,KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
+                         KC_HOME,KC_END,  KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS
+),
+/* Keymap 2: Mac layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   `    |   1  |   2  |   3  |   4  |   5  | Ins  |           |  FN4 |   6  |   7  |   8  |   9  |   0  | Bcksp  |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Esc  |           |  "   |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | F10    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
+ * |--------+------+------+------+------+------| -/_  |           |  =/+ |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   ?  | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |LCtrl | LAlt |  Gui | Left | Right|                                       |  Up  | Down |   [  |   ]  | Del  |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,--------------.
+ *                                        | FN3  | Home |       | PgUp | FN3    |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | End  |       | PgDn |        |      |
+ *                                 | FN3  | Gui  |------|       |------|   `/   |Space/|
+ *                                 |      |      | Alt  |       | `    |   Alt  |RCtrl |
+ *                                 `--------------------'       `----------------------'
+ */
+// If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// Otherwise, it needs KC_*
+[MCLR] = KEYMAP(
+        // left hand
+        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_INS,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_ESC,
+        KC_F10,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_MINS,
+        KC_LCTL,        KC_LALT,      KC_LGUI,KC_LEFT,KC_RGHT,
+                                              KC_FN3, KC_HOME,
+                                                              KC_END,
+                                              KC_FN3, KC_LGUI,KC_LALT,
+        // right hand
+        KC_FN4,      KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_BSPC,
+        KC_QUOT,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
+                     KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_ENT,
+        KC_EQL,      KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+                             KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_DELT,
+        KC_PGUP,     KC_FN3,
+        KC_PGDN,
+        KC_GRV,      ALT_T(KC_GRV), CTL_T(KC_SPC)
+    ),
+/* Keymap 3: Mac Function Layer
+ *
+ * ,---------------------------------------------------.           ,--------------------------------------------------.
+ * |         |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+ * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
+ * |         |      |      | VolD | VolU | Mute |      |           |      |      |      |      |      |      | Pause  |
+ * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |  CAPS   |      |      |      |      |      |------|           |------| Home | PgUp | PgDn | End  |      |  Prt   |
+ * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |         |      |      |      |      |      |      |           |      | Stop | Play | Prev | Next |      |        |
+ * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |       |      |      |      |      |                                       |      |      |      |      |      |
+ *   `-----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// SYMBOLS
+[MCFN] = KEYMAP(
+       // left hand
        KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F11,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_VOLD,KC_VOLU,KC_MUTE,KC_TRNS,
        KC_CAPS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -102,10 +189,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
-/* Keymap 2: Gaming layer
+/* Keymap 4: Gaming layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  | Ins  |           |  FN2 |   6  |   7  |   8  |   9  |   0  | Bcksp  |
+ * |   `    |   1  |   2  |   3  |   4  |   5  | Ins  |           |  FN7 |   6  |   7  |   8  |   9  |   0  | Bcksp  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Esc  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -116,7 +203,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |LCtrl |  Gui | LAlt | LAlt | LAlt |                                       |  Up  | Down |   [  |   ]  | Del  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,--------------.
- *                                        | FN1  | Home |       | PgUp | FN1    |
+ *                                        | FN6  | Home |       | PgUp | FN6    |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 |Space | LCtrl|------|       |------| RCtrl  |Space |
@@ -125,27 +212,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[GAME] = KEYMAP(  // layer 0 : default
+[GAME] = KEYMAP(
         // left hand
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_INS,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_ESC,
         KC_F10,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_MINS,
         KC_LCTL,        KC_LGUI,      KC_LALT,KC_LALT,KC_LALT,
-                                              KC_FN3, KC_HOME,
+                                              KC_FN6, KC_HOME,
                                                               KC_END,
                                               KC_SPC, KC_LCTL,KC_LALT,
         // right hand
-        KC_FN4,      KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_BSPC,
+        KC_FN7,      KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_BSPC,
         KC_QUOT,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                      KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_ENT,
         KC_EQL,      KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                              KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_DELT,
-        KC_PGUP,     KC_FN3,
+        KC_PGUP,     KC_FN6,
         KC_PGDN,
         KC_GRV,      KC_RCTL,CTL_T(KC_SPC)
     ),
-/* Keymap 3: Game Function Layer
+/* Keymap 5: Game Function Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |         |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |        |
@@ -192,9 +279,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM fn_actions[] = {
     [1] = ACTION_LAYER_MOMENTARY(BAFN),                // FN1 - Momentary Layer 1 (Base Functions)
-    [2] = ACTION_LAYER_SET(GAME, ON_PRESS),            // FN2 - Switch to Layer 2 (Gaming Layer)
-    [3] = ACTION_LAYER_MOMENTARY(GAFN),                // FN3 - Momentary Layer 1 (Gaming Layer Functions)
-    [4] = ACTION_LAYER_SET(BASE, ON_PRESS)             // FN4 - Reset to Base Layer
+    [2] = ACTION_LAYER_SET(MCLR, ON_PRESS),            // FN2 - Switch to Layer 2 (Mac Layer)
+    [3] = ACTION_LAYER_MOMENTARY(MCFN),                // FN3 - Momentary Layer 3 (MacLayer Functions)
+    [4] = ACTION_LAYER_SET(BASE, ON_PRESS),            // FN4 - Reset to Base Layer
+    [5] = ACTION_LAYER_SET(GAME, ON_PRESS),            // FN5 - Switch to Layer 4 (Game Layer)
+    [6] = ACTION_LAYER_MOMENTARY(GAFN),                // FN6 - Momentary Layer 5 (Game Layer Functions)
+    [7] = ACTION_LAYER_SET(BASE, ON_PRESS)             // FN7 - Reset to Base Layer
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -262,9 +352,9 @@ void matrix_scan_user(void) {
         case 1:
             break;
         case 2:
-            ergodox_right_led_3_on();
+            ergodox_right_led_2_on();
             break;
-        case 3:
+        case 4:
             ergodox_right_led_3_on();
             break;
         default:
