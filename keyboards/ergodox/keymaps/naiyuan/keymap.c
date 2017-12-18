8c87b75e1cade72c399d4b8f509369446def6394
Naiyuan Zhang <pirateblanc@gmail.com>
2017-12-18 00:39:29 -0800

    Updating gaming layer mappings for gw2.

--- keyboards/ergodox/keymaps/naiyuan/keymap.c
+++ keyboards/ergodox/keymaps/naiyuan/keymap.c
@@ -197,13 +197,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Esc  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | F10    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
- * |--------+------+------+------+------+------| -/_  |           |  =/+ |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| FN6  |           |  =/+ |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LCtrl |  Gui | LAlt | LAlt | LAlt |                                       |  Up  | Down |   [  |   ]  | Del  |
+ *   |LCtrl |  Gui | RAlt | LAlt | LAlt |                                       |  Up  | Down |   [  |   ]  | Del  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,--------------.
- *                                        | FN6  | Home |       | PgUp | FN6    |
+ *                                        | -/_  | Home |       | PgUp | FN6    |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 |Space | LCtrl|------|       |------| RCtrl  |Space |
@@ -217,9 +217,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_INS,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_ESC,
         KC_F10,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_MINS,
-        KC_LCTL,        KC_LGUI,      KC_LALT,KC_LALT,KC_LALT,
-                                              KC_FN6, KC_HOME,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_FN6,
+        KC_LCTL,        KC_LGUI,      KC_RALT,KC_LALT,KC_LALT,
+                                              KC_MINS,KC_HOME,
                                                               KC_END,
                                               KC_SPC, KC_LCTL,KC_LALT,
         // right hand
@@ -237,9 +237,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |         |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |      |      | VolD | VolU | Mute |      |           |      |      |      |      |      |      | Pause  |
+ * |         |  6   |  7   |  8   |  9   |  0   |      |           |      | VolD | VolU | Mute |      |      | Pause  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  CAPS   |      |      |      |      |      |------|           |------| Home | PgUp | PgDn | End  |      |  Prt   |
+ * |  CAPS   |  [   |  ]   |      |      |      |------|           |------| Home | PgUp | PgDn | End  |      |  Prt   |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |      |      |      |      |      |      |           |      | Stop | Play | Prev | Next |      |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -257,8 +257,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [GAFN] = KEYMAP(
        // left hand
        KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F11,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_VOLD,KC_VOLU,KC_MUTE,KC_TRNS,
-       KC_CAPS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_TRNS,
+       KC_CAPS,KC_LBRC,KC_RBRC,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
@@ -266,7 +266,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_F12,  KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_PAUS,
+       KC_TRNS, KC_VOLD, KC_VOLU,KC_MUTE, KC_TRNS, KC_TRNS, KC_PAUS,
                 KC_HOME, KC_PGDN,KC_PGUP, KC_END,  KC_TRNS, KC_PSCR,
        KC_TRNS, KC_MSTP, KC_MPLY,KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
                          KC_HOME,KC_END,  KC_TRNS, KC_TRNS, KC_TRNS,
